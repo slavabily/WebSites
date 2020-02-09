676d309c91bb7b232271896276223d75d03b8234
slavabily <slavabily@me.com>
2020-02-09 18:33:06 +0200

    Initial Commit

---
+++ WebSites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebSites
+//
+//  Created by slava bily on 9/2/20.
+//  Copyright © 2020 slava bily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
